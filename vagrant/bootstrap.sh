3627881d439b121fe5e6152be6c4116b29a09930
KC Wayman <kc.wayman@appian.com>
2018-08-29 13:53:39 -0400

    fixup! no more ansible

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -5,6 +5,8 @@ set -eo pipefail
 # Get to the point where we can install other ppas
 apt-get update && apt-get -y install software-properties-common
 
-apt-add-repository ppa:ansible/ansible ppa:jonathonf/vim
+for repo in ppa:ansible/ansible ppa:jonathonf/vim; do
+  apt-add-repository $repo
+done
 
 apt-get update && apt-get -y install ansible vim
