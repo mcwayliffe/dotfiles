342ee1da165582197c875faac81c38dd727f2b49
KC Wayman <kc.wayman@appian.com>
2018-08-30 11:04:50 -0400

    fixup! fixup! fixup! no more ansible

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -12,26 +12,32 @@ done
 apt-get update && apt-get -y install ansible git vim zsh
 
 # --- Get latest tmux ---
-tmux_version="2.7"
-workdir="$(mktemp -d)"
-
 # If we have the OS package, remove it
 if apt list --installed 2>/dev/null | grep tmux; then
   apt-get -y purge tmux
 fi
 
-# Download newer version from tmux releases
-(cd ${workdir}; \
-  curl --silent --remote-name --location \
-    https://github.com/tmux/tmux/releases/download/${tmux_version}/tmux-${tmux_version}.tar.gz)
+# Build dependencies
+apt -y install automake build-essential libevent-dev libncurses5-dev pkg-config
 
-tar xf ${workdir}/tmux-${tmux_version}.tar.gz \
-  && rm ${workdir}/tmux-${tmux_version}.tar.gz
+# Download newer version from tmux releases
+tmux_version="2.7"
+workdir="$(mktemp -d)"
+
+cd ${workdir}
+
+curl --silent --remote-name --location \
+    https://github.com/tmux/tmux/releases/download/${tmux_version}/tmux-${tmux_version}.tar.gz
+
+tar xf tmux-${tmux_version}.tar.gz \
+  && rm tmux-${tmux_version}.tar.gz
 
 # Configure and install
-(cd ${workdir}/tmux-${tmux_version}; \
-  ./configure && make && make install)
+cd tmux-${tmux_version}
+./configure && make && make install
 
 # Setup sources
 rm -rf /usr/local/src/tmux-* \
   && mv ${workdir}/tmux-${tmux_version} /usr/local/src
+
+cd -
